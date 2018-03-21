c5646cf343acb8e90a82326e3a64c8dc5471f788
Cesar Carruitero <cesar@mozilla.pe>
2018-03-21 14:50:06 -0500

    add specs for validate imported product

--- spec/solidus_datashift/product_importer_spec.rb
+++ spec/solidus_datashift/product_importer_spec.rb
@@ -9,6 +9,45 @@ describe SolidusDataShift::ProductImporter do
     it 'should successfull upload products' do
       expect { importer.run }.to change { Spree::Product.count }.by(3)
     end
+
+    context 'after run importer' do
+      before do
+        importer.run
+        @product = Spree::Product.first
+      end
+
+      it 'has correct name' do
+        expect(@product.name).to eq('Demo Product for AR Loader')
+      end
+
+      it 'has correct sku' do
+        expect(@product.sku).to eq('DEMO_001')
+      end
+
+      it 'has correct price' do
+        expect(@product.price.to_s).to eq('399.99')
+      end
+
+      it 'has correct cost_price' do
+        expect(@product.cost_price.to_s).to eq('320.0')
+      end
+
+      it 'has correct shipping_category' do
+        expect(@product.shipping_category.name).to eq('small')
+      end
+
+      xit 'has correct taxons' do
+      end
+
+      xit 'has correct product properties' do
+      end
+
+      xit 'has correct variants' do
+      end
+
+      xit 'has correct count_on_hand' do
+      end
+    end
   end
 
   context 'with xls file' do
